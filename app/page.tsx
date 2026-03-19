1a72f7018f88d5c044329a2a26a76aa46a40e0b7
김태현 <143389711+taehyun00@users.noreply.github.com>
2026-03-19 17:05:04 +0900

    feat : header 퍼블리싱

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/common/header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div>
       <Header/>
     </div>
   );
--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,28 +1,27 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/public/logo_white.svg";
+import logo from "@/public/svg/logo_white.svg";
 
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-black dark:bg-black">
-      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+      <div className="container mx-auto flex h-28 items-center justify-between px-24">
         
         <Link href="/" className="text-xl font-bold tracking-tighter">
-          <Image src={logo} alt="Logo" width={100} height={100} />
+          <Image src={logo} alt="Logo" width={100} height={100} className="w-[20%] h-auto" priority />
         </Link>
 
         
-        <nav className="flex items-center gap-4">
-          <Link href="/login" className="text-sm font-medium hover:underline text-white">
-            일정확인
+        <nav className="flex items-center gap-24">
+          <Link href="/checkschedule" className="">
+            <p className="font-pretendard text-h2 font-semibold text-white">일정확인</p>
+          </Link>
+          <Link href="/calendar" className="">  
+            <p className="font-pretendard text-h2 font-semibold text-white">강습일정</p>
           </Link>
 
-          <Link href="/login" className="text-sm font-medium hover:underline text-white">
-            강습일정
-          </Link>
-
-          <Link href="/login" className="text-sm font-medium hover:underline text-white">
-            로그인
+          <Link href="/login" className="">
+            <p className="font-pretendard text-h2 font-semibold text-white">로그인</p>
           </Link>
         </nav>
       </div>
